255632bf97ef8c11b3bf7d0c5df8793319a4ec2f
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-06-11 18:06:45 -0300

    feat: create cities cards

--- src/components/BannerPage.tsx
+++ src/components/BannerPage.tsx
@@ -1,14 +1,10 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react"
+import { Flex, Text } from "@chakra-ui/react"
 
 export default function BannerPage() {
   return (
     <Flex
       height={500}
       w="100%"
-      // bgImage="url('https://images.unsplash.com/photo-1485081669829-bacb8c7bb1f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')"
-      // bgPosition="center"
-      // bgRepeat="no-repeat"
-      // bgSize="cover"
       _before={{
         content: '""',
         position: "absolute",
--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -4,6 +4,8 @@ import BannerPage from "../../components/BannerPage"
 import { Header } from "../../components/Header"
 
 export default function Continent() {
+  const img =
+    "https://images.unsplash.com/photo-1533676802871-eca1ae998cd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1933&q=80"
   return (
     <Box>
       <Head>
@@ -12,17 +14,78 @@ export default function Continent() {
       <Header />
       <BannerPage />
 
-      <Box p="2rem" maxWidth={1160} mx="auto">
-        <Text
-          textAlign="center"
-          color="dark.text"
-          fontSize="2.25rem"
-          fontWeight="500"
-          mt="3.5rem"
-        >
-          Vamos nessa? <br />
-          Então escolha seu continente
+      <Box p="2rem" maxWidth={1160} mx="auto" mt="5rem">
+        <Flex alignItems="center">
+          <Text
+            flex="1"
+            color="dark.text"
+            fontSize="1.5rem"
+            fontWeight="400"
+            textAlign="justify"
+          >
+            Apesar de não contar com um território muito extenso, a Europa é um
+            continente que possui grande influência social e cultural nos demais
+            continentes, afinal foi lá que surgiram os principais idiomas
+            falados ao redor do mundo.
+          </Text>
+
+          <HStack flex="1" spacing="4rem" ml="4rem">
+            <Flex alignItems="center" flexDirection="column" fontWeight="600">
+              <Text color="highlight" fontSize="2rem">
+                50
+              </Text>
+              <Text>países</Text>
+            </Flex>
+
+            <Flex alignItems="center" flexDirection="column" fontWeight="600">
+              <Text color="highlight" fontSize="2rem">
+                60
+              </Text>
+              <Text>línguas</Text>
+            </Flex>
+
+            <Flex alignItems="center" flexDirection="column" fontWeight="600">
+              <Text color="highlight" fontSize="2rem">
+                27
+              </Text>
+              <Text>cidades +100</Text>
+            </Flex>
+          </HStack>
+        </Flex>
+
+        <Text color="dark.text" fontSize="2.25rem" fontWeight="500" mt="5rem">
+          Cidades +100
         </Text>
+
+        <Flex flexDirection="column" w={256} mt="2.5rem">
+          <Image src={img} borderRadius="4px 4px 0 0" />
+          <Flex
+            maxH="280px"
+            bg="light.white"
+            p="1.5rem"
+            justifyContent="space-between"
+            alignContent="center"
+            borderBottom="1px"
+            borderLeft="1px"
+            borderRight="1px"
+            borderColor="highlight50"
+            borderRadius="0 0 4px 4px"
+          >
+            <Box>
+              <Text fontWeight="600" fontSize="1.25rem">
+                Venice
+              </Text>
+              <Text fontWeight="500" fontSize="1rem" color="dark.info">
+                Itália
+              </Text>
+            </Box>
+            <Image
+              boxSize="30px"
+              src="https://upload.wikimedia.org/wikipedia/commons/0/03/Flag_of_Italy.svg"
+              borderRadius="full"
+            />
+          </Flex>
+        </Flex>
       </Box>
     </Box>
   )
