f6f23773cd280f8ec946f383ad081dfc8ead4929
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-06-11 14:31:06 -0300

    style: adjust max width

--- src/components/TravelTypes.tsx
+++ src/components/TravelTypes.tsx
@@ -2,7 +2,7 @@ import { Box, Flex, HStack, Image, Text } from "@chakra-ui/react"
 
 export default function TravelTypes({}) {
   return (
-    <Box maxWidth={1160} mx="auto" mt="7rem">
+    <Box mt="4.5rem">
       <HStack justifyContent="space-between" fontWeight={600} fontSize="1.5rem">
         <Flex flexDirection="column" alignItems="center" gap="1.5rem">
           <Image src="/assets/cocktail.svg" w={85} />
