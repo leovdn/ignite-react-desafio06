37ffbb444b5b1ef5f0261eba8a8d5c10c69a9e51
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-06-10 21:00:14 -0300

    feat: create theme globals

---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+  colors: {
+    highlight: "#FFBA08",
+    highlight50: "rgba(255, 186, 8, 0.5)",
+    dark: {
+      black: "#000",
+      text: "#47585B",
+      info: "#999999",
+      info50: "rgba(153, 153, 153, 0.5)",
+    },
+    light: {
+      white: "#fff",
+      text: "#F5F8FA",
+      info: "#DADADA",
+    },
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "light.text",
+        color: "dark.text",
+      },
+    },
+  },
+})
