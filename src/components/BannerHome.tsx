40d2409a7f6cbe41dc78287f2c85f341252a180f
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-06-13 13:47:27 -0300

    style: BannerHome component

--- src/components/BannerHome.tsx
+++ src/components/BannerHome.tsx
@@ -4,10 +4,11 @@ export default function BannerHome() {
   return (
     <Box
       bgImage="url('/assets/bg.png')"
+      maxH={["160px", "256px", "400px", "500px"]}
       bgPosition="center"
       bgRepeat="no-repeat"
       bgSize="cover"
-      px="2rem"
+      px={["1rem", "1.5rem"]}
     >
       <Flex
         maxWidth={1160}
@@ -17,15 +18,32 @@ export default function BannerHome() {
         py="1.5rem"
       >
         <Box flex="1">
-          <Text fontSize="2.25rem" color="light.text">
+          <Text
+            as="strong"
+            fontWeight="500"
+            fontSize={["1.25rem", "1.5rem", "1.75rem", "2.25rem"]}
+            color="light.text"
+          >
             5 Continentes, <br /> infinitas possibilidades.
           </Text>
-          <Text color="light.info" fontSize="1.25rem" mt="1.25rem">
-            Chegou a hora de tirar do papel a viagem que você <br /> sempre
-            sonhou.
-          </Text>
+          <Box
+            color="light.info"
+            fontSize={["0.875rem", "1rem", "1rem", "1.25rem"]}
+            mt=".5rem"
+          >
+            <Text display={["inline", "inline", "block"]}>
+              Chegou a hora de tirar do papel a viagem que você{" "}
+            </Text>
+            <Text display={["inline-block"]}>sempre sonhou.</Text>
+          </Box>
         </Box>
-        <Image src="/assets/airplane.svg" flex="1" maxW={400} mb="-3rem" />
+
+        <Image
+          src="/assets/airplane.svg"
+          flex="1"
+          maxW={[0, 0, 280, 360]}
+          mb={[0, 0, "-4rem", "-3rem"]}
+        />
       </Flex>
     </Box>
   )
