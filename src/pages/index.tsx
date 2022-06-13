c78dc5801613b31cdf8541d58ffc4e8fbdb48d3b
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-06-13 17:29:44 -0300

    style: responsive Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import BannerHome from "../components/BannerHome"
 import TravelTypes from "../components/TravelTypes/TravelTypes"
 import { continentsBanner } from "../utils/continentsBanner"
 import Head from "next/head"
-import Carousel from "../components/Swiper/SwiperContainer"
+import Carousel from "../components/Carousel/CarouselContainer"
 
 const Home: NextPage = () => {
   const continents = continentsBanner
@@ -24,20 +24,28 @@ const Home: NextPage = () => {
       <Box px={["1rem", "1.5rem"]} maxWidth={1160} mx="auto">
         <TravelTypes />
 
-        <Divider borderColor="dark.text" mx="auto" mt="5rem" w="6rem" />
+        <Divider
+          borderColor="dark.text"
+          mx="auto"
+          mt={["2.25rem", "3rem", "4rem", "5rem"]}
+          w="6rem"
+        />
 
         <Text
           textAlign="center"
           color="dark.text"
-          fontSize="2.25rem"
+          fontSize={["1.25rem", "1.5rem", "2rem", "2.25rem"]}
           fontWeight="500"
-          mt="3.5rem"
+          mt={["1.5rem", "2rem", "3rem", "3.5rem"]}
         >
           Vamos nessa? <br />
           Então escolha seu continente
         </Text>
 
-        <Box h={450} mt="3.2rem">
+        <Box
+          h={["250px", "300px", "400px", "450px"]}
+          my={["1.25rem", "1.5rem", "2.25rem", "3.2rem"]}
+        >
           <Carousel data={continents} />
         </Box>
       </Box>
