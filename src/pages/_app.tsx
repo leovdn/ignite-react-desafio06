cd7618bc42b68df11865a6168ddb898646446211
Leonardo Vitor Dantas Nascimento <leo_vdn@hotmail.com>
2022-06-14 12:58:29 -0300

    feat: config prismic providers

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,12 +6,27 @@ import "swiper/css"
 import "swiper/css/pagination"
 import "swiper/css/navigation"
 import "../styles/swiper.css"
+import { PrismicProvider } from "@prismicio/react"
+import { PrismicPreview } from "@prismicio/next"
+import { linkResolver, repositoryName } from "../../prismicio"
+import Link from "next/link"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <PrismicProvider
+      linkResolver={linkResolver}
+      internalLinkComponent={({ href, children, ...props }) => (
+        <Link href={href}>
+          <a {...props}>{children}</a>
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <ChakraProvider theme={theme}>
+          <Component {...pageProps} />
+        </ChakraProvider>
+      </PrismicPreview>
+    </PrismicProvider>
   )
 }
 
